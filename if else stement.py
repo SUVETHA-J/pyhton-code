9076393dde355c5fcd613473e0e22efaeeacf631
SUVETHA-J <97973447+SUVETHA-J@users.noreply.github.com>
2022-01-22 16:28:13 +0530

    Add files via upload

---
+++ if else stement.py
@@ -0,0 +1,10 @@
+snows=int(input("enter a value 0 or 1"))
+if snows==0:
+    print("i will go out")
+else:
+    print("i will not")
+rains=int(input("enter a vallue"))
+if rains==1:
+    print(" i will dance")
+else:
+    print("i will not dance")
