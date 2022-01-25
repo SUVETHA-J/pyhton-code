416bc0a2fd7af40c17efb25d392da0bb8431bdfa
SUVETHA-J <97973447+SUVETHA-J@users.noreply.github.com>
2022-01-25 11:52:06 +0530

    Add files via upload
    1)step function of for loop
    2)add values to tuples

---
+++ assignment 2.2.py
@@ -0,0 +1,11 @@
+#step function for example let us see increement by 2
+for i in range(1,10,2):
+    print(i)# you will 1,3 5 likewise upto 10-1
+#we cant make a loop as infinite loop
+t=('1','2','3','4','5')
+s=list(t)
+s.append('6')
+print("list",s)
+s=tuple(s)
+print("tuple",s)
+
